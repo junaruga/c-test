581d02432b324d983b6128d41d6d117299ab9dc4
Jun Aruga <junaruga@users.noreply.github.com>
2023-07-27 18:59:02 +0200

    Add testing program to test gdb watch and display. (#9)

---
+++ test_gdb_watch/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a = 1;
+    int b = 1;
+
+    a = 2;
+    printf("Hello 1!\n");
+    a = 3;
+    printf("Hello 2!\n");
+
+    return 0;
+}
